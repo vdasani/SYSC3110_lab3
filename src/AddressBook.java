bbc12dae1f1ba9213026da6caafbe0d9b9f03d6f
vdasani63 <vdasani63@gmail.com>
2021-09-17 12:09:32 -0400

    Add a buddy to the address book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,6 +17,8 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy= new BuddyInfo("Vishesh", "Ottawa", "123456779");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
     }
 }
