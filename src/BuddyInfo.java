3de0cf564adc1ce956899bb73dd817c2972d08bb
vdasani63 <vdasani63@gmail.com>
2021-09-23 19:38:57 -0400

    The buddy info class for the project

---
+++ src/BuddyInfo.java
@@ -0,0 +1,34 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String number;
+
+    public BuddyInfo() {
+        this("Christina", "Ottawa", "1234567890");
+    }
+
+    public BuddyInfo(String name, String address, String number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo();
+        buddy.name = "Christina";
+        System.out.println("Hello "+ buddy.getName());
+    }
+}
